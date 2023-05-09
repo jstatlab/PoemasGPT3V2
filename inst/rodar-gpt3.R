b00bb06da95b4c40cfb258c6984e51d07bc7e9ec
jstatlab <jsjale1983@gmail.com>
2023-05-09 16:55:47 -0300

    Sem tentar badge

--- inst/rodar-gpt3.R
+++ inst/rodar-gpt3.R
@@ -5,16 +5,8 @@ pkgload::load_all(".") # Carrega menos coisas que devtools::load_all(".")
 
 texto <- rodar_gpt3(token)
 
-text <- c(
-  "<!-- badges: start -->
-  [![R-CMD-check](https://github.com/jstatlab/PoemasGPT3V2/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/jstatlab/PoemasGPT3V2/actions/workflows/R-CMD-check.yaml)
-  <!-- badges: end -->",
-  text
-)
-
 cat(texto, file = "README.md")
 
 # Usar template setup-r-dependencies, pois
 # ela é uma action preparada para lidar com dependências
 # e que estamos trabalhando com pacote.
-#
