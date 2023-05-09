4d1cb622913689bd37089466520d079a0cc05264
jstatlab <jsjale1983@gmail.com>
2023-05-09 16:19:25 -0300

    Teste com badge no README.md

--- inst/rodar-gpt3.R
+++ inst/rodar-gpt3.R
@@ -5,6 +5,13 @@ pkgload::load_all(".") # Carrega menos coisas que devtools::load_all(".")
 
 texto <- rodar_gpt3(token)
 
+text <- c(
+  "<!-- badges: start -->
+  [![R-CMD-check](https://github.com/jstatlab/PoemasGPT3V2/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/jstatlab/PoemasGPT3V2/actions/workflows/R-CMD-check.yaml)
+  <!-- badges: end -->",
+  text
+)
+
 cat(texto, file = "README.md")
 
 # Usar template setup-r-dependencies, pois
